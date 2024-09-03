34dadea89c0c84b9f3574910ba9c0da8d963f9e3
JelleBouma <jelle@ecedo.com>
2024-09-03 23:29:50 +0200

    reduce epoll_ctl mods by listening to both EPOLLIN and EPOLLOUT

--- johnny_worker.c
+++ johnny_worker.c
@@ -71,60 +71,34 @@ hot void johnny_closes_connection(connection_context* ctx) {
     johnny_deallocates_connection(ctx);
 }
 
-hot ssize_t johnny_sends_bytes(connection_context* ctx, const char *response, const size_t response_length) {
-    ssize_t bytes_sent_cnt = 0;
-    while (bytes_sent_cnt < response_length) {
-        //const int flags = response_length - sentBytes > 10240 ? MSG_DONTWAIT | MSG_NOSIGNAL | MSG_ZEROCOPY : MSG_DONTWAIT | MSG_NOSIGNAL;
-        const int flags = MSG_DONTWAIT | MSG_NOSIGNAL;
-        const ssize_t write_res = send(ctx->fd, response + bytes_sent_cnt, response_length - bytes_sent_cnt, flags);
-        if (write_res == -1) {
-            if (errno != EAGAIN && errno != EWOULDBLOCK) {
-                johnny_closes_connection(ctx);
-                return -1;
-            }
-            break;
-        }
-        bytes_sent_cnt += write_res;
+hot ssize_t johnny_sends_bytes(connection_context* ctx) {
+    size_t* response_length = get_response_length(ctx);
+    const ssize_t bytes_sent_cnt = send(ctx->fd, ctx->response, *response_length, MSG_DONTWAIT | MSG_NOSIGNAL);
+    if (bytes_sent_cnt == -1) {
+        if (errno != EAGAIN && errno != EWOULDBLOCK)
+            return -1;
     }
-    return bytes_sent_cnt;
+    else {
+        ctx->response += bytes_sent_cnt;
+        *response_length -= bytes_sent_cnt;
+    }
+    return 0;
 }
 
-hot int johnny_sends_response(connection_context* ctx, const char* file_name, const int epfd) {
+hot int johnny_sends_response(connection_context* ctx, const char* file_name) {
     const cmph_uint32 index = cmph_search(JOHNNY_HASH, file_name, strlen(file_name));
     const johnny_file johnny_file = JOHNNY_FILES[index];
 
     // build response
     const bool found = !strcmp(file_name, johnny_file.url_encoded_file_name);
-    const char* response = found ? johnny_file.response : http_404;
-    const size_t response_length = found ? johnny_file.response_length : strlen(response);
+    ctx->response = found ? johnny_file.response : http_404;
+    ctx->response_length = found ? johnny_file.response_length : strlen(ctx->response);
 
     // send HTTP response to client
-    const size_t bytes_sent_cnt = johnny_sends_bytes(ctx, response, response_length);
-    if (bytes_sent_cnt == -1)
-        return -1;
-    if (bytes_sent_cnt < response_length) {
-        ctx->response = response + bytes_sent_cnt;
-        *get_response_length(ctx) = response_length - bytes_sent_cnt;
-        struct epoll_event ev = { .data.ptr = ctx, .events = EPOLLOUT | EPOLLET };
-        epoll_ctl(epfd, EPOLL_CTL_MOD, ctx->fd, &ev);
-        return EAGAIN;
-    }
-    return 0;
+    return johnny_sends_bytes(ctx);
 }
 
-hot void johnny_handles_requests(connection_context* ctx, const int epfd) {
-    //float startTime = (float)clock()/CLOCKS_PER_SEC;
-    size_t* response_length = get_response_length(ctx);
-    if (*response_length > 0) {
-        const ssize_t bytes_sent_cnt = johnny_sends_bytes(ctx, ctx->response, ctx->response_length);
-        if (bytes_sent_cnt == -1)
-            return;
-        *response_length -= bytes_sent_cnt;
-        if (*response_length == 0) {
-            struct epoll_event ev = { .data.ptr = ctx, .events = EPOLLIN | EPOLLET };
-            epoll_ctl(epfd, EPOLL_CTL_MOD, ctx->fd, &ev);
-        }
-    }
+hot void johnny_handles_requests(connection_context* ctx) {
     ssize_t bytes_received = 1;
     register int rnrnget_slash_counter = ctx->rnrnget_slash_counter;
     register int bytes_parsed = 1;
@@ -145,12 +119,11 @@ hot void johnny_handles_requests(connection_context* ctx, const int epfd) {
                 for (; bytes_parsed < bytes_received; bytes_parsed++) {
                     if (buffer[bytes_parsed] == ' ') { // end of file name
                         buffer[bytes_parsed] = '\0';
-                        //printf("johnny_handles_requests time to start responding in %f\r\n", (float)clock()/CLOCKS_PER_SEC - startTime); // 1 - 57 us
-                        const int succes_code = johnny_sends_response(ctx, buffer + bytes_parsed - rnrnget_slash_counter + 9, epfd);
+                        const int succes_code = johnny_sends_response(ctx, buffer + bytes_parsed - rnrnget_slash_counter + 9);
                         if (succes_code == -1)
                             return;
                         if (succes_code == EAGAIN) {
-                            memcpy(buffer, buffer + bytes_parsed, bytes_received - bytes_parsed);
+                            memmove(buffer, buffer + bytes_parsed, bytes_received - bytes_parsed);
                             ctx->rnrnget_slash_counter = 0;
                             ctx->buffer_remaining = bytes_received - bytes_parsed;
                             return;
@@ -162,7 +135,7 @@ hot void johnny_handles_requests(connection_context* ctx, const int epfd) {
                 }
                 if (rnrnget_slash_counter != 0 && rnrnget_slash_counter < 266) { // found only partial file name
                     if (bytes_parsed != rnrnget_slash_counter + 9)
-                        memcpy(buffer, buffer + bytes_parsed - rnrnget_slash_counter + 9, rnrnget_slash_counter - 9);
+                        memmove(buffer, buffer + bytes_parsed - rnrnget_slash_counter + 9, rnrnget_slash_counter - 9);
                 }
                 else
                     rnrnget_slash_counter = 0;
@@ -184,45 +157,34 @@ hot void johnny_works(const int* server_fd, const int epfd) {
     if (nfds < 0)
         perror("calling epoll_wait");
     for (int i = 0; i < nfds; i++) {
-        //float start_time = (float)clock()/CLOCKS_PER_SEC;
-        if (ev_buf[i].data.ptr == server_fd) {
+        struct epoll_event ev = ev_buf[i];
+        if (ev.data.ptr == server_fd) {
             const int fd = accept4(*server_fd, NULL, NULL, SOCK_NONBLOCK);
             if (fd != -1) { // connection made
-                // int yes = 1;
-                // if (setsockopt(fd, SOL_SOCKET, SO_ZEROCOPY, &yes, sizeof(yes)))
-                //     perror("calling setsockopt zerocopy");
-                //
-                // timer_ctx[connections].fd = timerfd_create(CLOCK_MONOTONIC, TFD_NONBLOCK);
-                //
-                // timerfd_settime(timer_ctx[connections].fd, 0, &timer_spec, NULL);
-                // timer_ctx[connections].handler = johnny_closes_connection;
-                //
-                // timer_ctx[connections].joint_context = &con_ctx[connections];
-                // con_ctx[connections].joint_context = &timer_ctx[connections];
-                //
-                // timer_ev[connections].data.ptr = &timer_ctx[connections];
-                // timer_ev[connections].events = EPOLLIN | EPOLLET;
-                // epoll_ctl(epfd, EPOLL_CTL_ADD, timer_ctx[connections].fd, &timer_ev[connections]);
-                connection_context* con = johnny_allocates_connection(fd);
-                struct epoll_event con_ev = { .data.ptr = con, .events = EPOLLIN | EPOLLET};
+                connection_context* ctx = johnny_allocates_connection(fd);
+                struct epoll_event con_ev = { .data.ptr = ctx, .events = EPOLLIN | EPOLLOUT | EPOLLET};
                 epoll_ctl(epfd, EPOLL_CTL_ADD, fd, &con_ev);
             }
-            //printf("johnny_listen fd %i time to handle in %f\r\n", fd, (float)clock()/CLOCKS_PER_SEC - start_time); // listen 5 - 29 us
         }
         else {
-            johnny_handles_requests(ev_buf[i].data.ptr, epfd);
-            //printf("johnny_worker fd %i time to handle in %f\r\n", ctx->fd, (float)clock()/CLOCKS_PER_SEC - startTime); // request 31 - 1127 us, timer 60 - 262 us
+            if (ev.events & EPOLLOUT && *get_response_length(ev.data.ptr) > 0) { // resume sending
+                const ssize_t bytes_sent_cnt = johnny_sends_bytes(ev.data.ptr);
+                if (bytes_sent_cnt == -1) { // error, cannot send data
+                    johnny_closes_connection(ev.data.ptr);
+                    return;
+                }
+            }
+            johnny_handles_requests(ev.data.ptr);
         }
     }
 }
 
 noreturn void johnny_worker(int server_fd, const int epfd) {
-    struct itimerspec timer_spec = { .it_interval = {.tv_sec = JOHNNY_INACTIVE_CONNECTION_TIMEOUT, .tv_nsec = 0}, .it_value = {.tv_sec = 5, .tv_nsec = JOHNNY_INACTIVE_CONNECTION_TIMEOUT}};
     struct epoll_event ev;
     ev.data.ptr = &server_fd;
     ev.events = EPOLLIN | EPOLLET;
     if (epoll_ctl(epfd, EPOLL_CTL_ADD, server_fd, &ev))
-        perror("calling epoll_ctl");
+        perror("calling epoll_ctl, adding the server fd");
     while (true)
         johnny_works(&server_fd, epfd);
 }
